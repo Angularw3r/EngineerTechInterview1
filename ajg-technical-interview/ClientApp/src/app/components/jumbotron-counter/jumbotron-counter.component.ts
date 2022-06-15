e6c87911eb9777536354e37981a1e3e5c999a2a5
Angularw3r <45684645+Angularw3r@users.noreply.github.com>
2022-06-15 12:11:56 +0100

    Add files via upload

--- ajg-technical-interview/ClientApp/src/app/components/jumbotron-counter/jumbotron-counter.component.ts
+++ ajg-technical-interview/ClientApp/src/app/components/jumbotron-counter/jumbotron-counter.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, observable, Subscription } from 'rxjs';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-jumbotron-counter',
   templateUrl: './jumbotron-counter.component.html'
 })
-export class JumbotronCounterComponent {
+export class JumbotronCounterComponent implements OnInit {
+  public currentCount: number;
+  private subs = new Subscription();
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.displayCounter();
+  }
+  
+  displayCounter() {
+    this.subs.add(
+      this.dataService.getCounter().subscribe(c => this.currentCount = c))
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+  }
+
 }
